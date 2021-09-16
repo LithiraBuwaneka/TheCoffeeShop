d22707f9c8e2eff7a20c3ad4e5a343232ae72621
LithiraBuwaneka <lithirabuwaneka11@gmail.com>
2021-09-16 08:59:18 +0530

    ✔️ Review Section : Added

--- JS/script.js
+++ JS/script.js
@@ -17,3 +17,27 @@ document.querySelectorAll('.image-slider img').forEach(images =>{
         document.querySelector('.main-home-image').src = src;
     };
 });
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+          slidesPerView: 0,
+          
+        },
+        768: {
+          slidesPerView: 2,
+       
+        },
+      
+    },
+    loop : true,
+    grabCursor: true,
+  });
